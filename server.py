0c8c55717ef32351b2d44d6fe1c0c22e7e850aaf
sarmaakondi <avnmsarma@gmail.com>
2024-12-23 19:06:26 +1100

    Create contacts get route and send static contact list

--- server.py
+++ server.py
@@ -2,10 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+contacts = [
+    {"name": "Sarma", "phone": "123-456-7890"},
+    {"name": "John", "phone": "234-966-1275"},
+    {"name": "Bob", "phone": "456-789-0123"},
+    {"name": "Charlie", "phone": "345-678-9012"},
+]
+
 
 @app.get("/hello")
 def root():
     return {"message": "Hello from Flask!"}
+
+
+@app.get("/contacts")
+def list_contacts():
+    return contacts
 
 
 if __name__ == "__main__":
